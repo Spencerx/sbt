sbt/librarymanagement

Replace tuple with proper record type for licenses

core/src/main/contraband-scala/sbt/librarymanagement/LibraryManagementCodec.scala
@@ -49,6 +49,7 @@ trait LibraryManagementCodec extends sjsonnew.BasicJsonProtocol
   with sbt.librarymanagement.SftpRepositoryFormats
   with sbt.librarymanagement.ResolverFormats
   with sbt.librarymanagement.ModuleConfigurationFormats
+  with sbt.librarymanagement.LicenseInfoFormats
   with sbt.librarymanagement.ScmInfoFormats
   with sbt.librarymanagement.ModuleInfoFormats
   with sbt.librarymanagement.IvyFileConfigurationFormats

core/src/main/contraband-scala/sbt/librarymanagement/ModuleInfo.scala
@@ -10,13 +10,14 @@ final class ModuleInfo private (
   val description: String,
   val homepage: Option[java.net.URL],
   val startYear: Option[Int],
-  val licenses: Vector[scala.Tuple2[String, java.net.URL]],
+  val licenses: Vector[sbt.librarymanagement.LicenseInfo],
   val organizationName: String,
   val organizationHomepage: Option[java.net.URL],
   val scmInfo: Option[sbt.librarymanagement.ScmInfo],
   val developers: Vector[sbt.librarymanagement.Developer]) extends Serializable {
   
   private def this(nameFormal: String) = this(nameFormal, "", None, None, Vector.empty, "", None, None, Vector.empty)
+  private def this(nameFormal: String, description: String, homepage: Option[java.net.URL], startYear: Option[Int], organizationName: String, organizationHomepage: Option[java.net.URL], scmInfo: Option[sbt.librarymanagement.ScmInfo], developers: Vector[sbt.librarymanagement.Developer]) = this(nameFormal, description, homepage, startYear, Vector.empty, organizationName, organizationHomepage, scmInfo, developers)
   
   override def equals(o: Any): Boolean = this.eq(o.asInstanceOf[AnyRef]) || (o match {
     case x: ModuleInfo => (this.nameFormal == x.nameFormal) && (this.description == x.description) && (this.homepage == x.homepage) && (this.startYear == x.startYear) && (this.licenses == x.licenses) && (this.organizationName == x.organizationName) && (this.organizationHomepage == x.organizationHomepage) && (this.scmInfo == x.scmInfo) && (this.developers == x.developers)
@@ -28,7 +29,7 @@ final class ModuleInfo private (
   override def toString: String = {
     "ModuleInfo(" + nameFormal + ", " + description + ", " + homepage + ", " + startYear + ", " + licenses + ", " + organizationName + ", " + organizationHomepage + ", " + scmInfo + ", " + developers + ")"
   }
-  private[this] def copy(nameFormal: String = nameFormal, description: String = description, homepage: Option[java.net.URL] = homepage, startYear: Option[Int] = startYear, licenses: Vector[scala.Tuple2[String, java.net.URL]] = licenses, organizationName: String = organizationName, organizationHomepage: Option[java.net.URL] = organizationHomepage, scmInfo: Option[sbt.librarymanagement.ScmInfo] = scmInfo, developers: Vector[sbt.librarymanagement.Developer] = developers): ModuleInfo = {
+  private[this] def copy(nameFormal: String = nameFormal, description: String = description, homepage: Option[java.net.URL] = homepage, startYear: Option[Int] = startYear, licenses: Vector[sbt.librarymanagement.LicenseInfo] = licenses, organizationName: String = organizationName, organizationHomepage: Option[java.net.URL] = organizationHomepage, scmInfo: Option[sbt.librarymanagement.ScmInfo] = scmInfo, developers: Vector[sbt.librarymanagement.Developer] = developers): ModuleInfo = {
     new ModuleInfo(nameFormal, description, homepage, startYear, licenses, organizationName, organizationHomepage, scmInfo, developers)
   }
   def withNameFormal(nameFormal: String): ModuleInfo = {
@@ -43,7 +44,7 @@ final class ModuleInfo private (
   def withStartYear(startYear: Option[Int]): ModuleInfo = {
     copy(startYear = startYear)
   }
-  def withLicenses(licenses: Vector[scala.Tuple2[String, java.net.URL]]): ModuleInfo = {
+  def withLicenses(licenses: Vector[sbt.librarymanagement.LicenseInfo]): ModuleInfo = {
     copy(licenses = licenses)
   }
   def withOrganizationName(organizationName: String): ModuleInfo = {
@@ -62,5 +63,6 @@ final class ModuleInfo private (
 object ModuleInfo {
   
   def apply(nameFormal: String): ModuleInfo = new ModuleInfo(nameFormal)
-  def apply(nameFormal: String, description: String, homepage: Option[java.net.URL], startYear: Option[Int], licenses: Vector[scala.Tuple2[String, java.net.URL]], organizationName: String, organizationHomepage: Option[java.net.URL], scmInfo: Option[sbt.librarymanagement.ScmInfo], developers: Vector[sbt.librarymanagement.Developer]): ModuleInfo = new ModuleInfo(nameFormal, description, homepage, startYear, licenses, organizationName, organizationHomepage, scmInfo, developers)
+  def apply(nameFormal: String, description: String, homepage: Option[java.net.URL], startYear: Option[Int], organizationName: String, organizationHomepage: Option[java.net.URL], scmInfo: Option[sbt.librarymanagement.ScmInfo], developers: Vector[sbt.librarymanagement.Developer]): ModuleInfo = new ModuleInfo(nameFormal, description, homepage, startYear, organizationName, organizationHomepage, scmInfo, developers)
+  def apply(nameFormal: String, description: String, homepage: Option[java.net.URL], startYear: Option[Int], licenses: Vector[sbt.librarymanagement.LicenseInfo], organizationName: String, organizationHomepage: Option[java.net.URL], scmInfo: Option[sbt.librarymanagement.ScmInfo], developers: Vector[sbt.librarymanagement.Developer]): ModuleInfo = new ModuleInfo(nameFormal, description, homepage, startYear, licenses, organizationName, organizationHomepage, scmInfo, developers)
 }

ivy/src/main/scala/sbt/internal/librarymanagement/Ivy.scala
@@ -313,7 +313,7 @@ final class IvySbt(
         mod.setHomePage(h.toString)
       }
       moduleInfo.licenses foreach { l =>
-        mod.addLicense(new License(l._1, l._2.toString))
+        mod.addLicense(new License(l.name, l.url.toString))
       }
       IvySbt.addConfigurations(mod, configurations)
       IvySbt.addArtifacts(mod, module.explicitArtifacts)

Spencerx/sbt

lm-core/src/main/contraband-scala/sbt/librarymanagement/LibraryManagementCodec.scala
@@ -49,6 +49,7 @@ trait LibraryManagementCodec extends sbt.librarymanagement.ConfigRefFormats
   with sbt.librarymanagement.SftpRepositoryFormats
   with sbt.librarymanagement.ResolverFormats
   with sbt.librarymanagement.ModuleConfigurationFormats
+  with sbt.librarymanagement.LicenseFormats
   with sbt.librarymanagement.ScmInfoFormats
   with sbt.librarymanagement.ModuleInfoFormats
   with sbt.librarymanagement.IvyFileConfigurationFormats

lm-core/src/main/contraband-scala/sbt/librarymanagement/ModuleInfo.scala
@@ -10,13 +10,14 @@ final class ModuleInfo private (
   val description: String,
   val homepage: Option[java.net.URI],
   val startYear: Option[Int],
-  val licenses: Vector[scala.Tuple2[String, java.net.URI]],
+  val licenses: Vector[sbt.librarymanagement.License],
   val organizationName: String,
   val organizationHomepage: Option[java.net.URI],
   val scmInfo: Option[sbt.librarymanagement.ScmInfo],
   val developers: Vector[sbt.librarymanagement.Developer]) extends Serializable {
   
   private def this(nameFormal: String) = this(nameFormal, "", None, None, Vector.empty, "", None, None, Vector.empty)
+  private def this(nameFormal: String, description: String, homepage: Option[java.net.URI], startYear: Option[Int], organizationName: String, organizationHomepage: Option[java.net.URI], scmInfo: Option[sbt.librarymanagement.ScmInfo], developers: Vector[sbt.librarymanagement.Developer]) = this(nameFormal, description, homepage, startYear, Vector.empty, organizationName, organizationHomepage, scmInfo, developers)
   
   override def equals(o: Any): Boolean = this.eq(o.asInstanceOf[AnyRef]) || (o match {
     case x: ModuleInfo => (this.nameFormal == x.nameFormal) && (this.description == x.description) && (this.homepage == x.homepage) && (this.startYear == x.startYear) && (this.licenses == x.licenses) && (this.organizationName == x.organizationName) && (this.organizationHomepage == x.organizationHomepage) && (this.scmInfo == x.scmInfo) && (this.developers == x.developers)
@@ -28,7 +29,7 @@ final class ModuleInfo private (
   override def toString: String = {
     "ModuleInfo(" + nameFormal + ", " + description + ", " + homepage + ", " + startYear + ", " + licenses + ", " + organizationName + ", " + organizationHomepage + ", " + scmInfo + ", " + developers + ")"
   }
-  private def copy(nameFormal: String = nameFormal, description: String = description, homepage: Option[java.net.URI] = homepage, startYear: Option[Int] = startYear, licenses: Vector[scala.Tuple2[String, java.net.URI]] = licenses, organizationName: String = organizationName, organizationHomepage: Option[java.net.URI] = organizationHomepage, scmInfo: Option[sbt.librarymanagement.ScmInfo] = scmInfo, developers: Vector[sbt.librarymanagement.Developer] = developers): ModuleInfo = {
+  private def copy(nameFormal: String = nameFormal, description: String = description, homepage: Option[java.net.URI] = homepage, startYear: Option[Int] = startYear, licenses: Vector[sbt.librarymanagement.License] = licenses, organizationName: String = organizationName, organizationHomepage: Option[java.net.URI] = organizationHomepage, scmInfo: Option[sbt.librarymanagement.ScmInfo] = scmInfo, developers: Vector[sbt.librarymanagement.Developer] = developers): ModuleInfo = {
     new ModuleInfo(nameFormal, description, homepage, startYear, licenses, organizationName, organizationHomepage, scmInfo, developers)
   }
   def withNameFormal(nameFormal: String): ModuleInfo = {
@@ -43,7 +44,7 @@ final class ModuleInfo private (
   def withStartYear(startYear: Option[Int]): ModuleInfo = {
     copy(startYear = startYear)
   }
-  def withLicenses(licenses: Vector[scala.Tuple2[String, java.net.URI]]): ModuleInfo = {
+  def withLicenses(licenses: Vector[sbt.librarymanagement.License]): ModuleInfo = {
     copy(licenses = licenses)
   }
   def withOrganizationName(organizationName: String): ModuleInfo = {
@@ -62,5 +63,6 @@ final class ModuleInfo private (
 object ModuleInfo {
   
   def apply(nameFormal: String): ModuleInfo = new ModuleInfo(nameFormal)
-  def apply(nameFormal: String, description: String, homepage: Option[java.net.URI], startYear: Option[Int], licenses: Vector[scala.Tuple2[String, java.net.URI]], organizationName: String, organizationHomepage: Option[java.net.URI], scmInfo: Option[sbt.librarymanagement.ScmInfo], developers: Vector[sbt.librarymanagement.Developer]): ModuleInfo = new ModuleInfo(nameFormal, description, homepage, startYear, licenses, organizationName, organizationHomepage, scmInfo, developers)
+  def apply(nameFormal: String, description: String, homepage: Option[java.net.URI], startYear: Option[Int], organizationName: String, organizationHomepage: Option[java.net.URI], scmInfo: Option[sbt.librarymanagement.ScmInfo], developers: Vector[sbt.librarymanagement.Developer]): ModuleInfo = new ModuleInfo(nameFormal, description, homepage, startYear, organizationName, organizationHomepage, scmInfo, developers)
+  def apply(nameFormal: String, description: String, homepage: Option[java.net.URI], startYear: Option[Int], licenses: Vector[sbt.librarymanagement.License], organizationName: String, organizationHomepage: Option[java.net.URI], scmInfo: Option[sbt.librarymanagement.ScmInfo], developers: Vector[sbt.librarymanagement.Developer]): ModuleInfo = new ModuleInfo(nameFormal, description, homepage, startYear, licenses, organizationName, organizationHomepage, scmInfo, developers)
 }

lm-ivy/src/main/scala/sbt/internal/librarymanagement/Ivy.scala
@@ -332,7 +332,7 @@ final class IvySbt(
         mod.setHomePage(h.toString)
       }
       moduleInfo.licenses foreach { l =>
-        mod.addLicense(new License(l._1, l._2.toString))
+        mod.addLicense(new License(l.spdxId, l.uri.toString))
       }
       IvySbt.addConfigurations(mod, configurations)
       IvySbt.addArtifacts(mod, module.explicitArtifacts)
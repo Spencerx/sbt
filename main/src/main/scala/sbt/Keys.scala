eea840be2c14ac102a3a424d8ea4d7c0728efa33
Matthew de Detrich <mdedetrich@gmail.com>
2021-07-04 15:09:59 +0200

    Replace tuple with proper record type for licenses

--- main/src/main/scala/sbt/Keys.scala
+++ main/src/main/scala/sbt/Keys.scala
@@ -381,7 +381,7 @@ object Keys {
   val description = settingKey[String]("Project description.").withRank(BSetting)
   val homepage = settingKey[Option[URI]]("Project homepage.").withRank(BSetting)
   val startYear = settingKey[Option[Int]]("Year in which the project started.").withRank(BMinusSetting)
-  val licenses = settingKey[Seq[(String, URI)]]("Project licenses as (name, url) pairs.").withRank(BMinusSetting)
+  val licenses = settingKey[Seq[License]]("Project licenses as (name, url) pairs.").withRank(BMinusSetting)
   val organization = settingKey[String]("Organization/group ID.").withRank(APlusSetting)
   val organizationName = settingKey[String]("Organization full/formal name.").withRank(BMinusSetting)
   val organizationHomepage = settingKey[Option[URI]]("Organization homepage.").withRank(BMinusSetting)
