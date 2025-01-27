2ee5eb7fa7740cc65f92537a282b2b064c13b0d0
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-01-27 11:34:26 -0500

    Make timing outputs consistently show hours and hint at time format
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- main-command/src/main/scala/sbt/internal/client/NetworkClient.scala
+++ main-command/src/main/scala/sbt/internal/client/NetworkClient.scala
@@ -1130,13 +1130,13 @@ object NetworkClient {
     val totalString = s"$total s" +
       (if (total <= 60) ""
        else {
-         val maybeHours = total / 3600 match {
-           case 0 => ""
-           case h => f"$h%02d:"
+         val hours = total / 3600 match {
+           case 0 => "0"
+           case h => f"$h%02d"
          }
          val mins = f"${total % 3600 / 60}%02d"
          val secs = f"${total % 60}%02d"
-         s" ($maybeHours$mins:$secs)"
+         s" ($hours:$mins:$secs.0)"
        })
     s"Total time: $totalString, completed $nowString"
   }
