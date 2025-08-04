38606b1f224e6208e6390a3a9d338dbc6def85e4
Eugene Yokota <eed3si9n@gmail.com>
2025-08-04 01:15:20 -0400

    Bump Zinc

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
 
   // sbt modules
   private val ioVersion = nightlyVersion.getOrElse("1.10.5")
-  val zincVersion = nightlyVersion.getOrElse("2.0.0-M4")
+  val zincVersion = nightlyVersion.getOrElse("2.0.0-M5")
 
   private val sbtIO = "org.scala-sbt" %% "io" % ioVersion
 
