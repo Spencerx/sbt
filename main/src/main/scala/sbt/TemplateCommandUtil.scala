7c53ba434acd4b4e323575870772d7a987feb81b
xuwei-k <6b656e6a69@gmail.com>
2025-08-26 09:02:25 +0900

    remove sbt-projectmatrix in template

--- main/src/main/scala/sbt/TemplateCommandUtil.scala
+++ main/src/main/scala/sbt/TemplateCommandUtil.scala
@@ -339,7 +339,6 @@ lazy val core = (projectMatrix in file("core"))
     IO.write(new File("build.sbt"), content)
 
     val pluginsContent = """
-addSbtPlugin("com.eed3si9n" % "sbt-projectmatrix" % "0.10.0")
 addSbtPlugin("org.scala-js" % "sbt-scalajs" % "1.17.0")
 addSbtPlugin("org.scala-native" % "sbt-scala-native" % "0.5.5")
 """
