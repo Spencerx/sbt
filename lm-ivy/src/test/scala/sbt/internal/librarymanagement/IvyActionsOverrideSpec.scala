e63a4b8f8c1b1a7e25dfe127a7741b70b2b24354
mkdev11 <jaysmth689@gmail.com>
2026-01-11 01:40:51 +0200

    Refactor applyDependencyOverrides into testable function

--- lm-ivy/src/test/scala/sbt/internal/librarymanagement/IvyActionsOverrideSpec.scala
+++ lm-ivy/src/test/scala/sbt/internal/librarymanagement/IvyActionsOverrideSpec.scala
@@ -5,11 +5,9 @@ import org.scalatest.matchers.should.Matchers
 
 class IvyActionsOverrideSpec extends AnyFlatSpec with Matchers {
 
-  "applyOverridesToDeliveredIvy XML transformation" should "replace rev attribute for matching dependencies" in {
-    // Simulate the override map
+  "IvyActions.applyDependencyOverrides" should "replace rev attribute for matching dependencies" in {
     val overrideMap = Map(("org.slf4j", "slf4j-api") -> "2.0.16")
 
-    // Sample Ivy XML with "managed" version (as reported in issue #7951)
     val sampleXml =
       <ivy-module version="2.0">
         <info organisation="test" module="test" revision="1.0"/>
@@ -19,44 +17,8 @@ class IvyActionsOverrideSpec extends AnyFlatSpec with Matchers {
         </dependencies>
       </ivy-module>
 
-    // Apply the same transformation logic as in IvyActions
-    val updated =
-      new scala.xml.transform.RuleTransformer(new scala.xml.transform.RewriteRule {
-        override def transform(n: scala.xml.Node): Seq[scala.xml.Node] = n match {
-          case e @ scala.xml.Elem(prefix, "dependency", attrs, scope, children*) =>
-            val org = attrs.get("org").map(_.text).getOrElse("")
-            val name = attrs.get("name").map(_.text).getOrElse("")
-            overrideMap.get((org, name)) match {
-              case Some(overrideRev) =>
-                // Build new attributes by replacing 'rev' attribute value
-                def updateAttrs(metadata: scala.xml.MetaData): scala.xml.MetaData = {
-                  metadata match {
-                    case scala.xml.Null => scala.xml.Null
-                    case attr if attr.key == "rev" =>
-                      new scala.xml.UnprefixedAttribute(
-                        "rev",
-                        overrideRev,
-                        updateAttrs(attr.next)
-                      )
-                    case attr =>
-                      attr.copy(next = updateAttrs(attr.next))
-                  }
-                }
-                scala.xml.Elem(
-                  prefix,
-                  "dependency",
-                  updateAttrs(attrs),
-                  scope,
-                  minimizeEmpty = true,
-                  children*
-                )
-              case None => e
-            }
-          case other => other
-        }
-      }).transform(sampleXml).head
+    val updated = IvyActions.applyDependencyOverrides(sampleXml, overrideMap)
 
-    // Verify the transformation
     val dependencies = (updated \\ "dependency")
 
     // Check slf4j-api has overridden version
@@ -83,40 +45,7 @@ class IvyActionsOverrideSpec extends AnyFlatSpec with Matchers {
         </dependencies>
       </ivy-module>
 
-    val updated =
-      new scala.xml.transform.RuleTransformer(new scala.xml.transform.RewriteRule {
-        override def transform(n: scala.xml.Node): Seq[scala.xml.Node] = n match {
-          case e @ scala.xml.Elem(prefix, "dependency", attrs, scope, children*) =>
-            val org = attrs.get("org").map(_.text).getOrElse("")
-            val name = attrs.get("name").map(_.text).getOrElse("")
-            overrideMap.get((org, name)) match {
-              case Some(overrideRev) =>
-                def updateAttrs(metadata: scala.xml.MetaData): scala.xml.MetaData = {
-                  metadata match {
-                    case scala.xml.Null => scala.xml.Null
-                    case attr if attr.key == "rev" =>
-                      new scala.xml.UnprefixedAttribute(
-                        "rev",
-                        overrideRev,
-                        updateAttrs(attr.next)
-                      )
-                    case attr =>
-                      attr.copy(next = updateAttrs(attr.next))
-                  }
-                }
-                scala.xml.Elem(
-                  prefix,
-                  "dependency",
-                  updateAttrs(attrs),
-                  scope,
-                  minimizeEmpty = true,
-                  children*
-                )
-              case None => e
-            }
-          case other => other
-        }
-      }).transform(sampleXml).head
+    val updated = IvyActions.applyDependencyOverrides(sampleXml, overrideMap)
 
     val dep = (updated \\ "dependency").head
     (dep \ "@org").text shouldBe "org.example"
@@ -137,40 +66,7 @@ class IvyActionsOverrideSpec extends AnyFlatSpec with Matchers {
         </dependencies>
       </ivy-module>
 
-    val updated =
-      new scala.xml.transform.RuleTransformer(new scala.xml.transform.RewriteRule {
-        override def transform(n: scala.xml.Node): Seq[scala.xml.Node] = n match {
-          case e @ scala.xml.Elem(prefix, "dependency", attrs, scope, children*) =>
-            val org = attrs.get("org").map(_.text).getOrElse("")
-            val name = attrs.get("name").map(_.text).getOrElse("")
-            overrideMap.get((org, name)) match {
-              case Some(overrideRev) =>
-                def updateAttrs(metadata: scala.xml.MetaData): scala.xml.MetaData = {
-                  metadata match {
-                    case scala.xml.Null => scala.xml.Null
-                    case attr if attr.key == "rev" =>
-                      new scala.xml.UnprefixedAttribute(
-                        "rev",
-                        overrideRev,
-                        updateAttrs(attr.next)
-                      )
-                    case attr =>
-                      attr.copy(next = updateAttrs(attr.next))
-                  }
-                }
-                scala.xml.Elem(
-                  prefix,
-                  "dependency",
-                  updateAttrs(attrs),
-                  scope,
-                  minimizeEmpty = true,
-                  children*
-                )
-              case None => e
-            }
-          case other => other
-        }
-      }).transform(sampleXml).head
+    val updated = IvyActions.applyDependencyOverrides(sampleXml, overrideMap)
 
     val dep = (updated \\ "dependency").head
     (dep \ "@rev").text shouldBe "1.0.0" // Should remain unchanged
